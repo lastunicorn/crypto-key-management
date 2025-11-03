6cc5272b1c66c2af6e795ba9bfe611363045c83a
Alexandru Iuga <alex.iuga@gmail.com>
2025-11-03 16:56:43 +0200

    added a plugins page

---
+++ sources/CryptoKeyManagement.Wpf.Presentation/PluginsPage/PluginsPage.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows.Controls;
+
+namespace DustInTheWind.CryptoKeyManagement.Wpf.Presentation.PluginsPage;
+
+/// <summary>
+/// Interaction logic for PluginsPage.xaml
+/// </summary>
+public partial class PluginsPage : UserControl
+{
+    public PluginsPage()
+    {
+        InitializeComponent();
+    }
+}
