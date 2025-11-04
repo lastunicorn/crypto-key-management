3edc30be36bff0b4399690cde0a7589722165da1
Alexandru Iuga <alex.iuga@gmail.com>
2025-11-04 13:48:57 +0200

    Removed the "Inactive" label

--- sources/CryptoKeyManagement.Wpf.Presentation/PluginsPage/BooleanToVisibilityConverter.cs
+++ sources/CryptoKeyManagement.Wpf.Presentation/PluginsPage/BooleanToVisibilityConverter.cs
@@ -1,20 +1,24 @@
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace DustInTheWind.CryptoKeyManagement.Wpf.Presentation.PluginsPage;
 
-public class BoolToActiveStatusConverter : IValueConverter
+public class BooleanToVisibilityConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        if (value is bool isActive)
-            return isActive ? "Active" : "Inactive";
+        if (value is bool boolValue)
+            return boolValue ? Visibility.Visible : Visibility.Collapsed;
 
-        return "Unknown";
+        return Visibility.Collapsed;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        if (value is Visibility visibility)
+            return visibility == Visibility.Visible;
+
+        return false;
     }
 }
